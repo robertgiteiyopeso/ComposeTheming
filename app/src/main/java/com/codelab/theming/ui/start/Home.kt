e56e34b9e3567b921d2036a5c7392bd1b52460f7
Robert Gitei <robert.gitei@yopeso.com>
2022-08-31 18:20:24 +0300

    6. Working with Text

--- app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -34,6 +34,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.codelab.theming.R
@@ -101,6 +102,7 @@ fun Header(
     ) {
         Text(
             text = text,
+            style = MaterialTheme.typography.subtitle2,
             modifier = modifier
                 .fillMaxWidth()
                 .semantics { heading() }
@@ -152,6 +154,9 @@ private fun PostMetadata(
 ) {
     val divider = "  •  "
     val tagDivider = "  "
+    val tagStyle = MaterialTheme.typography.overline.toSpanStyle().copy(
+        background = MaterialTheme.colors.primary.copy(alpha = 0.1f)
+    )
     val text = buildAnnotatedString {
         append(post.metadata.date)
         append(divider)
@@ -159,7 +164,9 @@ private fun PostMetadata(
         append(divider)
         post.tags.forEachIndexed { index, tag ->
             if (index != 0) {
-                append(tagDivider)
+                withStyle(tagStyle) {
+                    append(tagDivider)
+                }
             }
             append(" ${tag.uppercase(Locale.getDefault())} ")
         }
